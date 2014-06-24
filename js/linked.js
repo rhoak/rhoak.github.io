00f8267b111261c32d8620c937bfa6ae8c211890
shnappley <rachaelhoak@gmail.com>
2014-06-24 11:56:41 -0400

    adding files

---
+++ js/linked.js
@@ -0,0 +1,12 @@
+(function(){ 
+  
+  /* var img = $(".img");
+	var width = $("#leftcol").width();
+	var ratio = img.height()/img.width();
+  var imgHeight = img.height()/ratio;
+
+	function resize() { 
+    img.css({"width" : width, "height" : imgHeight});
+} */
+
+})();
