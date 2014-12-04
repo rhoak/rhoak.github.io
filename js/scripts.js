10bc6c77e61f1cc692770e4244ac367173f90be3
rhoak <rachaelhoak@gmail.com>
2014-12-03 22:19:22 -0500

    update

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+(function($){
+  
+var setThumby = function(e) {
+    $('.thumby').on('mouseenter', function(e) {
+      var $k = $(this).find('.caption');
+      $k.removeClass('hidden-cap');
+      e.preventDefault();
+    });
+    $('.thumby').on('mouseleave', function(e) {
+      var $k = $(this).find('.caption');
+      $k.addClass('hidden-cap');
+      e.preventDefault();
+    });
+  };
+
+
+  setThumby();
+ 
+  $('.caption').addClass('hidden-cap');
+
+
+$(window).scroll(function() {
+    $('.graphbar').each(function(){
+    var imagePos = $(this).offset().top;
+
+    var topOfWindow = $(window).scrollTop();
+      if (imagePos < topOfWindow+500) {
+        $(this).addClass("stretchRight");
+      }
+    });
+  });
+
+  
+ /* $('.social').hover(function() {
+    $(this).toggleClass("pulse");
+  }); */
+
+  $('.cap-button').hover(function() {
+    $(this).toggleClass("pulse");
+  });
+
+
+
+
+
+})(jQuery);
