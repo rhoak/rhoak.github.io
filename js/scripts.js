eca8639ff8eacb632abbdb94bdbc44e964201c32
kashomon <jrhoak@gmail.com>
2014-12-06 20:17:27 -0700

    Add an example of late-loading the images.

--- js/scripts.js
+++ js/scripts.js
@@ -24,18 +24,38 @@ $(window).scroll(function() {
 
     var topOfWindow = $(window).scrollTop();
     if (imagePos < topOfWindow+500) {
-      $(this).addClass("stretchRight");
+      $(this).addClass('stretchRight');
     }
   });
 });
 
+// Dynamically loads images for modals.
+//
+// This expects there to be an <a> element with the following properties:
+//
+// class must include the zmdl-button class.
+// data-target: the ID ofthe modal div
+// img-url: The url of the img to load.
+// img-alt: The alt text of the image.
+$('.zmdl-button').click(function() {
+  var $b = $(this);
+  var modalId = $b.attr('data-target');
+  var imgUrl = $b.attr('img-url');
+  var imgAlt = $b.attr('img-alt');
+  var img = document.createElement("img");
+  img.setAttribute('src', imgUrl);
+  img.setAttribute('class', 'modal-img img-responsive img-center');
+  img.setAttribute('alt', imgAlt);
+  $(modalId + ' .modal-content').append(img)
+  $(modalId).modal();
+});
 
 /* $('.social').hover(function() {
   $(this).toggleClass("pulse");
 }); */
 
 $('.cap-button').hover(function() {
-  $(this).toggleClass("pulse");
+  $(this).toggleClass('pulse');
 });
 
 
